575048abbc092eebb38129ec6d969c486365fdaa
Eli Segal <eli.segal@gmail.com>
2019-01-29 00:23:13 +0200

    added org & general configs

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (exec-path-from-shell markdown-mode web-mode yaml-mode auto-complete counsel ace-window org-bullets which-key try use-package))))
+    (writegood-mode exec-path-from-shell markdown-mode web-mode yaml-mode auto-complete counsel ace-window org-bullets which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -13,31 +13,6 @@
  ;; If there is more than one, they won't work right.
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
 
-
-(setq inhibit-startup-message t)
-(if window-system
-    (tool-bar-mode -1)
-)
-
-(desktop-save-mode 1)
-(load-theme 'tsdh-light)
-(set-background-color "#FFFDFB")
-
-(setq-default indicate-empty-lines t)
-(when (not indicate-empty-lines)  
-  (toggle-indicate-empty-lines))
-
-(setq make-backup-files nil)
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-(setq echo-keystrokes 0.1
-      use-dialog-box nil
-      visible-bell t
-      ring-bell-function nil)
-(show-paren-mode t)
-
-
-(setq-default cursor-type 'bar) 
 
 ;; Melpa
 (require 'package)
@@ -59,84 +34,7 @@
 	(package-install 'use-package))
 
 
-(use-package exec-path-from-shell
-  :ensure t
-  :config
-  (exec-path-from-shell-initialize))
 
-(use-package try
-	:ensure t)
-
-(use-package which-key
-	:ensure t 
-	:config
-	(which-key-mode))
-
-(use-package org-bullets
-  :ensure t
-  :config
-  (add-hook 'org-mode-hook 'org-bullets-mode))
-
-(use-package ace-window
-  :ensure t
-  :init
-  (progn
-    (global-set-key [remap other-window] 'ace-window)
-    (custom-set-faces
-     '(aw-leading-char-face
-       ((t (:inherit ace-jump-face-foreground :height 3.0))))) 
-    ))
-
-;; (setq ido-enable-flex-matching t)
-;; (setq ido-everywhere t)
-;; (ido-mode 1)
-
-(defalias 'list-buffers 'ibuffer-other-window)
-
-;; it looks like counsel is a requirement for swiper
-(use-package counsel
-  :ensure t
-  )
-
-(use-package swiper
-  :ensure try
-  :config
-  (progn
-    (ivy-mode 1)
-    (setq ivy-use-virtual-buffers t)
-    (global-set-key "\C-s" 'swiper)
-    (global-set-key (kbd "C-c C-r") 'ivy-resume)
-    (global-set-key (kbd "<f6>") 'ivy-resume)
-    (global-set-key (kbd "M-x") 'counsel-M-x)
-    (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-    (global-set-key (kbd "<f1> f") 'counsel-describe-function)
-    (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-    (global-set-key (kbd "<f1> l") 'counsel-load-library)
-    (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-    (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-    (global-set-key (kbd "C-c g") 'counsel-git)
-    (global-set-key (kbd "C-c j") 'counsel-git-grep)
-    (global-set-key (kbd "C-c k") 'counsel-ag)
-    (global-set-key (kbd "C-x l") 'counsel-locate)
-    (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-    ))
-
-(use-package avy
-  :ensure t
-  :bind ("M-s" . avy-goto-char))
-
-(use-package auto-complete
-  :ensure t
-  :init
-  (progn
-    (ac-config-default)
-    (global-auto-complete-mode t)
-    ))
-
-;; Auto save
-(setq auto-save-visited-file-name t)
-
-(set-face-attribute 'default nil :family "Menlo" :height 130)
-
+(require 'init-startup)
 (require 'init-lang)
+(require 'init-org)
