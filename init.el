28217d4891af836ab5b14524022da73c8caf7e38
Eli Segal <eli.segal@gmail.com>
2019-02-05 00:30:20 +0200

    Added line numbers

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (nlinum-relative powerline smart-mode-line-powerline-theme git-gutter magit writegood-mode exec-path-from-shell markdown-mode web-mode yaml-mode auto-complete counsel ace-window org-bullets which-key try use-package))))
+    (evil nlinum-relative powerline smart-mode-line-powerline-theme git-gutter magit writegood-mode exec-path-from-shell markdown-mode web-mode yaml-mode auto-complete counsel ace-window org-bullets which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
